00aed28adf2a6221682058761007f732c5792d90
itsosmx <o.h1277@yahoo.com>
2023-02-01 18:24:36 +0200

    main

--- src/routes/main/index.jsx
+++ src/routes/main/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../../assets/logo.png";
+import "./style.css";
+import { NavLink } from "react-router-dom";
 export default function Main() {
   return (
-    <div>Main</div>
-  )
+    <div className="main-container">
+      <img src={Logo} alt="Muslim Bank" />
+      <h1>Muslim Bank - بنك المسلم</h1>
+      <NavLink to="/privacy">Privacy</NavLink>
+    </div>
+  );
 }
